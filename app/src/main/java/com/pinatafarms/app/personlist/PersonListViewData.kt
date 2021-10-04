3493f731b81be2479fde43af1665c18fb76037f6
Maksym Syniutka <maksym.synyutka@gmail.com>
2021-10-04 14:59:35 +0300

    Added progress visualization

--- app/src/main/java/com/pinatafarms/app/personlist/PersonListViewData.kt
+++ app/src/main/java/com/pinatafarms/app/personlist/PersonListViewData.kt
@@ -1,6 +1,4 @@
-package com.pinatafarms.app.persondetails
-
-import android.graphics.Bitmap
+package com.pinatafarms.app.personlist
 
 /**
  * View data class that represents UI state.
@@ -8,8 +6,6 @@ import android.graphics.Bitmap
  * If any more complicated logic would apply, please switch to using Kotlin Flows or LiveData within this class to deliver
  * changes to UI, so the entire UI will not rebuild, but only a specific part of it which reflects the given change.
  **/
-data class PersonDetailsViewData(
-    val thumbnail: Bitmap,
-    val showErrorMessage: Boolean = false,
-    val errorMessage: String = ""
+data class PersonListViewData(
+    val isLoading: Boolean = false
 )
